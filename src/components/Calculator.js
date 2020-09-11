ccf457522326c7829ff7f6ecad2aef466bc9a74a
Atinder-Pal <atty.pal46@gmail.com>
2020-09-11 16:08:45 -0600

    added code to track the state of input field 1 ad displayed it in result for validation;just solidifying the concept of state and practicing in small steps

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,13 +1,24 @@
-import React from 'react';
+import React, {useState} from 'react';
+
 
 function Calculator(props)
 {
+    //Set up for state of input1
+    const [newInput1, setNewInput1] = useState( '' );
+
+    //Defining Function for "onSubmit" form event
+    const doCalculation = ( e ) => {
+        e.preventDefault();
+
+        setNewInput1( document.getElementById('input1').value );
+    }
+
     return(
         <>
             <h1>{props.heading}</h1>
-            <form>
+            <form onSubmit={doCalculation}>
                 <label htmlFor='input1'>Input 1 </label><br/>
-                <input type='number' id='input1'/><br />
+                <input type='number' id='input1' /><br />
                                 
                 <label htmlFor="operation">Choose an operation to perform: <br />
                     <select  id="operation">
@@ -19,12 +30,12 @@ function Calculator(props)
                 </label><br />
             
                 <label htmlFor='input2'>Input 2 </label><br />
-                <input type='number' id='input2'/> 
-                   
+                <input type='number' id='input2'/>                    
                 <br /><br />
+
                 <input type= 'submit' id='calculate'value='Calculate'/>
                 <p>
-                    <strong> Result:</strong>
+                    <strong> Result:{newInput1}</strong>
                 </p>
             </form>
         </>
