7523ba889a642132c96d896e0dfb107aa5b353c3
Atinder-Pal <atty.pal46@gmail.com>
2020-09-11 15:23:06 -0600

    created basic JSX elements and rendered them via index;

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -2,7 +2,34 @@ import React from 'react';
 
 function Calculator()
 {
-    return(<> </>);
+    return(
+        <>
+            <h1>Welcome  to my Calculator App</h1>
+            <form>
+                <label htmlFor='input1'>Input 1 </label><br/>
+                <input type='text' id='input1'/><br />
+                                
+                <label htmlFor="operation">Choose an operation to perform: <br />
+                    <select  id="operation">
+                        <option value="addition"> &nbsp;&nbsp; + </option>
+                        <option value="subtraction">&nbsp;&nbsp;&nbsp; - </option>
+                        <option value="multiplication">&nbsp;&nbsp;&nbsp; * </option>
+                        <option value="division">&nbsp;&nbsp;&nbsp; / </option>
+                    </select>
+                </label><br />
+            
+                <label htmlFor='input'>Input 2 </label><br />
+                <input type='text' id='input2'/> 
+                   
+                <br /><br />
+                <input type= 'submit' id='calculate'value='Calculate'/>
+                <p>
+                    <strong> Result:</strong>
+                </p>
+            </form>
+        </>
+    );
+    
 }
 
 export default Calculator;
